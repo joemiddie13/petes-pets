1af75ab85547f1319b81c12f3fa07a3372194e4d
joemiddie13 <joemiddie13@gmail.com>
2024-05-23 11:14:12 -0700

    Users can search pets by name and breed

--- routes/pets.js
+++ routes/pets.js
@@ -31,6 +31,16 @@ module.exports = (app) => {
     });
   });
 
+  // SEARCH PET
+  app.get('/search', (req, res) => {
+    term = new RegExp(req.query.term, 'i');
+
+    Pet.find({ $or: [{'name': term}, {'species': term}] }).exec((err, pets) => {
+      res.render('pets-index', { pets: pets });
+    });
+  });
+
+
   // EDIT PET
   app.get('/pets/:id/edit', (req, res) => {
     Pet.findById(req.params.id).exec((err, pet) => {
