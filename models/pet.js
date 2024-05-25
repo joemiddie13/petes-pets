7ec575cfb6e0e4fd497c5245d371e3dff9365c75
joemiddie13 <joemiddie13@gmail.com>
2024-05-24 20:37:30 -0700

    Implemented pagination on the home page and search

--- models/pet.js
+++ models/pet.js
@@ -1,19 +1,22 @@
 "use strict";
 
-const mongoose = require('mongoose'),
-        Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const Schema = mongoose.Schema;
 
 const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
-  timestamps: true
+    name: { type: String, required: true },
+    species: { type: String },
+    birthday: { type: Date },
+    picUrl: { type: String },
+    picUrlSq: { type: String },
+    favoriteFood: { type: String },
+    description: { type: String }
+}, {
+    timestamps: true
 });
+
+mongoosePaginate.paginate.options = { limit: 3 };
+PetSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Pet', PetSchema);
