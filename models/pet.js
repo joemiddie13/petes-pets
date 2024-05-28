2eca8670c248169d89d549a3dbb1208d7d265043
joemiddie13 <joemiddie13@gmail.com>
2024-05-27 22:19:29 -0400

    added index

--- models/pet.js
+++ models/pet.js
@@ -6,19 +6,26 @@ const Schema = mongoose.Schema;
 
 const PetSchema = new Schema({
     name: { type: String, required: true },
-    birthday: {type: String, required: true },
+    birthday: { type: String, required: true },
     species: { type: String, required: true },
     picUrl: { type: String },
     picUrlSq: { type: String },
     avatarUrl: { type: String, required: true },
     favoriteFood: { type: String, required: true },
-    price: {type: Number, required: true},
+    price: { type: Number, required: true },
     description: { type: String, minlength: 140, required: true }
-  }, {
+}, {
     timestamps: true
-  });
-  
+});
 
+// Adding text index without weights
+PetSchema.index({ name: 'text', species: 'text', favoriteFood: 'text', description: 'text' });
+
+// Adding text index with weights
+PetSchema.index(
+    { name: 'text', species: 'text', favoriteFood: 'text', description: 'text' },
+    { name: 'My text index', weights: { name: 10, species: 4, favoriteFood: 2, description: 1 } }
+);
 
 mongoosePaginate.paginate.options = { limit: 3 };
 PetSchema.plugin(mongoosePaginate);
